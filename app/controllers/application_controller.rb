e87a1b325944a6fc8670297c6c5cf389cffb0456
Thais Mayara <thaismayara.tmsb@gmail.com>
2020-08-26 21:59:58 -0300

    devise sem controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nome, :ra, :email, :celular])
+  end
 end
