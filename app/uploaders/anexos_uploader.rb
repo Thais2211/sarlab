beafe5005b8ecede0e966acc61b3c4b98c60a2cf
Thais Mayara <thaismayara.tmsb@gmail.com>
2021-09-06 16:40:17 -0300

    caminho uploads

--- app/uploaders/anexos_uploader.rb
+++ app/uploaders/anexos_uploader.rb
@@ -11,8 +11,8 @@ class AnexosUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     #"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    #"/var/www/meriva.td.utfpr.edu.br/shared/public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    "C:/projetos-rails/anexos/#{mounted_as}/#{model.id}"
+    "/var/www/meriva.td.utfpr.edu.br/shared/public/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    #"C:/projetos-rails/anexos/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
